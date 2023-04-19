06def9f90678cf6fe1998882ee3e4d087da8a05a
Vladimir Eremeev <v.eremeev@samsung.com>
2023-04-19 13:14:30 +0300

    Explicitly specify constructor arguments in creation of FullMask
    To support model tracing and ONNX export

--- fast_transformers/transformers.py
+++ fast_transformers/transformers.py
@@ -129,7 +129,7 @@ class TransformerEncoder(Module):
         # Normalize the masks
         N = x.shape[0]
         L = x.shape[1]
-        attn_mask = attn_mask or FullMask(L, device=x.device)
+        attn_mask = attn_mask or FullMask(N=L, M=L, device=x.device)
         length_mask = length_mask or \
             LengthMask(x.new_full((N,), L, dtype=torch.int64))
 
